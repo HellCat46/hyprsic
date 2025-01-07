5bbe3c38953ed223b3bc10bcd33d32eabeae85e5
HellCat46 <hellcat2388@gmail.com>
2025-01-07 22:11:42 +0530

    Add GetWorkspaces Call to Unix Socket

--- src/Hyprland/Workspaces.cpp
+++ src/Hyprland/Workspaces.cpp
@@ -3,14 +3,27 @@
 #include "sys/un.h"
 #include "unistd.h"
 #include "cstdlib"
+#include "json/value.h"
+
+struct Workspace {
+  int id, monitorId;
+  std::string name, monitor;
+  bool fullScreen;
+};
+
 class HyprWorkspaces {
   private:
   std::string sockPath;
   int evtSockfd;
   int workSockfd;
+  Workspace* activeWorkspace;
+  Workspace allWorkspace;
 
+  int getPath();
 
   public:
+
+  int GetWorkspaces();
     int Init(){
       if(getPath()){
         return 1;
@@ -42,35 +55,38 @@ class HyprWorkspaces {
         return 1;
       }
 
-      char msg[] = "/dispatch workspace 6";
-      char buffer[1024];
-      std::cout<<write(workSockfd, msg, sizeof(msg))<<std::endl;
-      read(workSockfd, buffer, sizeof(buffer));
-      std::cout<<buffer<<std::endl;
-
-
-      return 0;
-    }
-
-    int getPath(){
-      char* runtimeDir = std::getenv("XDG_RUNTIME_DIR");
-      if(runtimeDir == nullptr){
-        std::cerr<<"XDG_RUNTIME_DIR is not set"<<std::endl;
-        return 1;
-      }
-
-      char* HIS = std::getenv("HYPRLAND_INSTANCE_SIGNATURE");
-      if(HIS == nullptr){
-        std::cerr<<"HYPRLAND_INSTANCE_SIGNATURE is not set"<<std::endl;
-        return 1;
-      }
-
-      sockPath.append(runtimeDir).append("/hypr/").append(HIS).append("/");
       return 0;
     }
 
   ~HyprWorkspaces(){
       close(evtSockfd);
       close(workSockfd);
-    }
+  }
 };
+
+int HyprWorkspaces::getPath(){
+  char* runtimeDir = std::getenv("XDG_RUNTIME_DIR");
+  if(runtimeDir == nullptr){
+    std::cerr<<"XDG_RUNTIME_DIR is not set"<<std::endl;
+    return 1;
+  }
+
+  char* HIS = std::getenv("HYPRLAND_INSTANCE_SIGNATURE");
+  if(HIS == nullptr){
+    std::cerr<<"HYPRLAND_INSTANCE_SIGNATURE is not set"<<std::endl;
+    return 1;
+  }
+
+  sockPath.append(runtimeDir).append("/hypr/").append(HIS).append("/");
+  return 0;
+}
+
+int HyprWorkspaces::GetWorkspaces(){
+  char msg[] = "j/workspaces";
+  char buffer[8192];
+  std::cout<<write(workSockfd, msg, sizeof(msg))<<std::endl;
+  read(workSockfd, buffer, sizeof(buffer));
+  std::cout<<buffer<<std::endl;
+
+  return 0;
+}
